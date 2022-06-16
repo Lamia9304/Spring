abceb9abd2c725dc597ec4cc99a1ebfaa629ce0d
Lamia9304 <43948789+Lamia9304@users.noreply.github.com>
2022-06-16 15:27:06 +0900

    글쓰기 데이터 mysql 확인

--- src/main/java/com/jangwon/board/controller/BoardController.java
+++ src/main/java/com/jangwon/board/controller/BoardController.java
@@ -1,12 +1,20 @@
 package com.jangwon.board.controller;
 
+import com.jangwon.board.dto.BoardDTO;
+import com.jangwon.board.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
-
-    @Controller
+@Controller
     public class BoardController{
+
+        private BoardService boardService;
+
+        public BoardController(BoardService boardService) {
+            this.boardService = boardService;
+        }
 
         @GetMapping("/")
         public String list(){
@@ -16,6 +24,14 @@ import org.springframework.web.bind.annotation.GetMapping;
         public String post(){
             return "board/post.html";
         }
+
+        @PostMapping("/post")
+        public String write(BoardDTO boardDTO)
+        {
+            boardService.savePost(boardDTO);
+            return "redirect:/";
+        }
+
 
     }
 
